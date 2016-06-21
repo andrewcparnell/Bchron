ba1d57135c4f3ea362301a1fbb30e0b6a82507c8
Andrew Parnell <andrew.parnell@gmail.com>
2016-06-21 10:46:16 +0100

    Missed the withPositions plottting function from latest Bchron update

--- R/plot.BchronCalibratedDates.R
+++ R/plot.BchronCalibratedDates.R
@@ -2,12 +2,12 @@ plot.BchronCalibratedDates <-
 function(x,
          withPositions=FALSE,
          pause=FALSE,
-         dateHeight = 30, 
+         dateHeight = 30,
          ...) {
-  
+
   # Get extra arguments if provided
   ex = list(...)#as.list(substitute(list(...)))[-1L]
-  
+
   if(is.null(ex$xlab)) ex$xlab = 'Age (cal years BP)'
   if(is.null(ex$ylab)) ex$ylab = ifelse(withPositions,'Position','Density')
 
@@ -21,10 +21,10 @@ function(x,
     if(is.null(ex$main)) ex$main = names(x)
     args = utils::modifyList(ex, list(...))
     do.call("plot", args)
-    #graphics::plot(ag,den,type='l',main=names(x),xlab=xlab,ylab=ylab,...)      
+    #graphics::plot(ag,den,type='l',main=names(x),xlab=xlab,ylab=ylab,...)
     graphics::mtext(paste(x[[1]]$calCurves),side=1,line=4,adj=0,cex=0.6)
   }
-  
+
   # Now for multiple dates without depths
   if(length(x)>1 & withPositions==FALSE) {
     for(i in 1:length(x)) {
@@ -40,7 +40,7 @@ function(x,
       if(pause) if(i<length(x)) readline('Hit Enter for next plot...')
     }
   }
-  
+
   # Finally for multiple dates with depths
   if(length(x)>1 & withPositions==TRUE) {
     xlimits = NULL
@@ -51,17 +51,17 @@ function(x,
     }
     #dateHeight=0.2*diff(pretty(ylimits))[1]
     ylimits[1] = ylimits[1]-dateHeight
-    if(is.null(ex$xlim)) ex$xlim = rev(xlimits) 
+    if(is.null(ex$xlim)) ex$xlim = rev(xlimits)
     if(is.null(ex$ylim)) ex$ylim = rev(ylimits)
     ex$x = ex$y = 1
     ex$type = 'n'
     if(is.null(ex$main)) ex$main = 'Calibrated dates by position'
     args = utils::modifyList(ex, list(...))
     do.call("plot", args)
-    
+
     for(i in 1:length(x)) {
-      graphics::polygon(x[[i]]$ageGrid,x[[i]]$positions-x[[i]]$densities*dateHeight/max(x[[i]]$densities),border=NA,col='gray')      
+      graphics::polygon(x[[i]]$ageGrid,x[[i]]$positions-x[[i]]$densities*dateHeight/max(x[[i]]$densities),border=NA,col='gray')
     }
   }
-  
+
 }
