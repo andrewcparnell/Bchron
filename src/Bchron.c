ea951a7990c7d2b781693b7007c2c4e7c04ea91f
Andrew Parnell <andrew.parnell@gmail.com>
2016-07-26 10:39:09 +0100

    Fixed issue with Bchron.c predicting too many depth events

--- src/Bchron.c
+++ src/Bchron.c
@@ -145,6 +145,9 @@ void predictInterp(double *alpha, double *lambda, double *beta, double *predictP
     double depthEvents[Nd];
     for(i=0;i<Nd;i++) depthEvents[i] = runif(*currPositionsj,*currPositionsjp1);
     R_rsort(depthEvents,Nd);
+  } else {
+    double depthEvents[1]
+    depthEvents = 0.0;
   }
   double timeEventsUnsc[Nd+1],timeEventsSum=0.0;
   for(i=0;i<Nd+1;i++) timeEventsUnsc[i] = rgamma(*alpha,1/(*beta));
