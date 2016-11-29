3fbbfbfdb5b22510c737e562269fbd62dfd9f81c
Andrew Parnell <andrew.parnell@gmail.com>
2016-11-29 17:32:43 +0000

    Updated vignette with apply for median

--- R/SampleAges.R
+++ R/SampleAges.R
@@ -1,17 +1,17 @@
-SampleAges = function(CalDates, n_samp = 1000) {
+SampleAges = function(CalDates, n_samp = 10000) {
   # Get a set of samples from the current set of dates
   x = CalDates
   if(class(x) != 'BchronCalibratedDates') stop('Object must be creaed from BchronCalibrate')
   n_dates = length(x)
-  out = matrix(NA, ncol = n_dates, nrow = n_samp) 
+  out = matrix(NA, ncol = n_dates, nrow = n_samp)
   colnames(out) = names(x)
-  
+
   for(i in 1:length(x)) {
     currdate = list(x=x[[i]]$ageGrid,y=x[[i]]$densities)
-    out[,i] = with(currdate, 
-                   sample(x, 
-                          size = n_samp, 
-                          replace = TRUE, 
+    out[,i] = with(currdate,
+                   sample(x,
+                          size = n_samp,
+                          replace = TRUE,
                           prob = y))
   }
   return(out)
