246a4e12406805ab45aefb432eb570eec66ca15d
Andrew Parnell <andrew.parnell@ucd.ie>
2018-07-05 11:55:59 +0100

    Updated calirbation curve example to include file.copy

--- R/CreateCalCurve.R
+++ R/CreateCalCurve.R
@@ -8,7 +8,7 @@
 #' @param one_sigma The one sigma (one standard deviation) values in uncalibrated units. If left blank it assumes these are all zero
 #' @param path_to_cal_curves The path to the calibration curves. Will write by default to the working directory
 #'
-#' @details All calibration curves are stored by Bchron in the standard R gzipped text format. You can find the location of the calibration curves by typing \code{system.file('data',package='Bchron')}. Any created calibration curve will be converted to this format. However R packages are not allowed to write to this directory so it is up to the user to put the resulting calibration curve file in the appropriate directory. It can then be used as in the examples below. However note that re-installing Bchron will likely over-write previously created calibration curves so you should make sure to store the code used to create it.
+#' @details All calibration curves are stored by Bchron in the standard R gzipped text format. You can find the location of the calibration curves by typing \code{system.file('data',package='Bchron')}. Any created calibration curve will be converted to this format. However R packages are not allowed to write to this directory so it is up to the user to put the resulting calibration curve file in the appropriate directory. It can then be used as in the examples below. However note that re-installing Bchron will likely over-write previously created calibration curves so you should make sure to store the code used to create it. As a short-cut to copying it by hand you can instead use the \code{file.copy} command in the example below.
 #'
 #' @seealso 
 #' \code{\link{BchronCalibrate}}
@@ -20,6 +20,11 @@
 #' intcal09 = read.table('http://www.radiocarbon.org/IntCal09%20files/intcal09.14c',sep=',')
 #' # Run CreateCalCurve
 #' CreateCalCurve(name='intcal09',cal_ages=intcal09[,1],uncal_ages=intcal09[,2],one_sigma=intcal09[,3])
+#' 
+#' # Copy the file to the right place
+#' file.copy(from = 'intcal09.rda',
+#'           to = system.file('data',package='Bchron'),
+#'                            overwrite = TRUE) # Only need this if you've run it more than once
 #' 
 #' # Calibrate the ages under two calibration curves
 #' age_09 = BchronCalibrate(age=15500, ageSds=150,
