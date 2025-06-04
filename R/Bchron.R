a7e06fe3b3c397a21ef68a2cdd62082d1a19541e
Andrew Parnell <Andrew.parnell1@ucd.ie>
2025-06-04 22:55:33 +0100

    Started updating bad files. But not finished

--- R/Bchron.R
+++ R/Bchron.R
@@ -5,6 +5,13 @@
 #' @section Bchron functions:
 #' The most important functions are \code{\link{BchronCalibrate}} to calibrate radiocarbon (and non-radiocarbon) dates, \code{\link{Bchronology}} for the age-depth model of Haslett and Parnell (2008), \code{\link{BchronRSL}} to get rate estimates for relative sea level data, \code{\link{BchronDensity}} and \code{\link{BchronDensityFast}} for non-parametric phase modelling of age data. See the help files for these functions for examples. See the vignette for more complete documentation
 #'
-#' @docType package
 #' @name Bchron
-NULL
+"_PACKAGE"
+
+utils::globalVariables(c("Age", "Density", "V1", "V2", "low", "high", "Density2",
+                         "dens", "age", "calMid", "c14Mid", "c14Low", "c14High",
+                         "calLow", "calHigh", "ageGrid", "densities", "height",
+                         "Date", "RSL", "ageErr", "rslErr", "predLow", "predHigh",
+                         "Rate", "rateLow", "rateHigh", "Acceleration", "accelLow",
+                         "accelHigh", "Age", "Date", "densities", "height",
+                         "positionLow", "positionHigh"))
--- cran-comments.md
+++ cran-comments.md
@@ -1,13 +1,11 @@
 ## Test environments
-* local OS X install, R 4.1.0
+* local OS X install, R 4.5.0
 * GitHub Actions
 * rhub
 * win-builder (devel and release)
 
 ## R CMD check results
 There were no ERRORs or WARNINGs or NOTEs on my local machine, GitHub Actions, or win-builder.
-
-I had some problems with some of the Rhub builds. It reported a success but gave 'Error: No such container: Bchron_4.7.6.tar.gz-f42c1d5fcd524be2a99f4ebde3c43d55-1' associated with Docker. I wasn't sure how to fix this. 
 
 Many thanks for your continuing important work with R,
 
